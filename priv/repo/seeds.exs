8ce2c7ce728efa64416c83043a52bdfb270275a5
Cody Brunner <me@codybrunner.com>
2022-01-08 04:55:27 -0500

    chore(priv): adds User seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,23 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias App.{Accounts, Repo}
+
+Repo.delete_all(Accounts.User)
+Repo.delete_all(Accounts.UserToken)
+
+users = [
+  %{
+    email: "turd@gmail.com",
+    password: "h3ll0_W0rld-123*"
+  },
+  %{
+    email: "jim-nasium@gmail.com",
+    password: "h3ll0_W0rld-123*"
+  }
+]
+
+for user <- users do
+  user
+  |> Accounts.register_user()
+end
